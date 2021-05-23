17a97c13ceb209a656905e09a18e29d80122b2c9
VasiaKara <vasiakara369@gmail.com>
2021-05-23 15:46:13 +0300

    Updates

---
+++ precache-manifest.6337ed667fe7ade63adf567709aa8737.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8645efee651621f1bff0352edc8bd5ec",
+    "url": "/my-AuctionApp/index.html"
+  },
+  {
+    "revision": "af7011f3fd173d4765fc",
+    "url": "/my-AuctionApp/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "77e5f8db80070dcea9b2",
+    "url": "/my-AuctionApp/static/js/2.9c0e32bb.chunk.js"
+  },
+  {
+    "revision": "af7011f3fd173d4765fc",
+    "url": "/my-AuctionApp/static/js/main.8ffd080b.chunk.js"
+  },
+  {
+    "revision": "235aba2b07c2785a4e48",
+    "url": "/my-AuctionApp/static/js/runtime-main.172daa92.js"
+  }
+]);
